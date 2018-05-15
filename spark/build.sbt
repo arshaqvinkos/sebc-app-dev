526b8f628da9f506093fb22cf601c8bd4dad7da8
Unknown <arshaq@vinkos.com>
2018-05-15 13:51:15 -0500

    Adding Spark Jobs

--- spark/build.sbt
+++ spark/build.sbt
@@ -1,7 +1,11 @@
 name := "SparkBatchExample"
+
 version := "1.0"
-scalaVersion := "2.x.x"
+
+scalaVersion := "2.11.8"
 
 javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
 
-// ADD CLOUDERA REPO AND DEPENDENCIES FOR SPARK APPS
+resolvers +=  "cloudera-repos" at "http://repository.cloudera.com/artifactory/cloudera-repos/"
+
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "2.3.0.cloudera2" % "provided")
--- spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
+++ spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
@@ -1,27 +1,34 @@
 package com.cloudera.sdk
 
-import org.apache.spark.{SparkConf, SparkContext }
+import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.{ FileSystem, Path }
 import org.apache.spark.rdd.RDD
 
 object SparkBatchExample {
-    def main(args: Array[String]): Unit = {
-        if (args.length < 1) {
-            System.err.println("Usage: SparkBatchExample <path>")
-            System.exit(1)
-        }
-
-        val path = args(0)
-        val conf = new SparkConf()
-            .setAppName("Spark Batch Example")
-            .setMaster("local[2]")
-            .set("spark.io.compression.codec", "lz4")
-        val sc = new SparkContext(conf)
- 
-        conf.set("spark.cleaner.ttl", "120000")
-
-        /* READ DATA FROM HDFS, SORT BY KEY, SAVE TO HDFS */
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      System.err.println("Usage: SparkBatchExample <path>")
+      System.exit(1)
     }
-}
 
+    val inpath = args(0)
+    val conf = new SparkConf()
+      .setAppName("Spark Batch Example")
+      .set("spark.io.compression.codec", "lz4")
+    val sc = new SparkContext(conf)
+
+    conf.set("spark.cleaner.ttl", "120000")
+
+    /* READ DATA FROM HDFS, SORT BY KEY, SAVE TO HDFS */
+
+    val file = sc.textFile(inpath)
+    val arrayTuples = file.map(line => line.split(",")).map(array => (array(0), array(1)))
+    val sortedrdd = arrayTuples.sortByKey()
+
+    val outpath = args(1)
+
+    sortedrdd.repartition(1).saveAsTextFile(outpath)
+
+  }
+}
